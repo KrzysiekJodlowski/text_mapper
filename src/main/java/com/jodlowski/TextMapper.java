d563325b2aab3c018264206ab88a0ddb0a9b9b3c
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-08-10 19:42:21 +0200

    Add fields to TextMapperTest class and initialize them Add constructor and mapWordsToCharacters method to TextMapper class

--- src/main/java/com/jodlowski/TextMapper.java
+++ src/main/java/com/jodlowski/TextMapper.java
@@ -1,4 +1,15 @@
 package com.jodlowski;
 
+import java.util.Map;
+import java.util.Set;
+
+
 public class TextMapper {
+
+    public TextMapper(String inputText) {
+    }
+
+    public Map<Character, Set<String>> mapWordsToCharacters() {
+        return null;
+    }
 }
--- src/test/java/com/jodlowski/TextMapperTest.java
+++ src/test/java/com/jodlowski/TextMapperTest.java
@@ -1,4 +1,35 @@
 package com.jodlowski;
 
+import org.junit.Before;
+import java.util.*;
+
+
 public class TextMapperTest {
+
+    private TextMapper textMapper;
+    private Map<Character, Set<String>> result;
+    private Map<Character, Set<String>> expected;
+
+    @Before
+    public void initializeMapper() {
+        this.textMapper = new TextMapper("Ala ma kota, kot KODUJE - w Javie kota!");
+        this.result = textMapper.mapWordsToCharacters();
+        this.expected = new LinkedHashMap<Character, Set<String>>() {
+            {
+                put('a', new TreeSet<>(Arrays.asList("ala", "javie", "kota", "ma")));
+                put('d', new TreeSet<>(Collections.singletonList("koduje")));
+                put('e', new TreeSet<>(Arrays.asList("javie", "koduje")));
+                put('i', new TreeSet<>(Collections.singletonList("javie")));
+                put('j', new TreeSet<>(Arrays.asList("javie", "koduje")));
+                put('k', new TreeSet<>(Arrays.asList("koduje", "kot", "kota")));
+                put('l', new TreeSet<>(Collections.singletonList("ala")));
+                put('m', new TreeSet<>(Collections.singletonList("ma")));
+                put('o', new TreeSet<>(Arrays.asList("koduje", "kot", "kota")));
+                put('t', new TreeSet<>(Arrays.asList("kot", "kota")));
+                put('u', new TreeSet<>(Collections.singletonList("koduje")));
+                put('v', new TreeSet<>(Collections.singletonList("javie")));
+                put('w', new TreeSet<>(Collections.singletonList("w")));
+            }
+        };
+    }
 }
